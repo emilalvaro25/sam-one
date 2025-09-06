038bb4c32ccf32f23e5c1c5fbad9693cf6f78324
Emil Danguilan <emilalvaro@aitekchat.com>
2025-09-06 13:56:29 +0800

    Update config.js

--- js/config/config.js
+++ js/config/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   API: {
-    KEY: "AI-EzaSyDHGlDAX5wzGJj6x77hZvTvWggGCgR97Ok",
+    KEY: "AIzaSyAaV0zMGC_BXbDQKcYR7yZFQL92CxEFlos",
     BASE_URL: "wss://generativelanguage.googleapis.com/ws",
     VERSION: "v1alpha",
     MODEL_NAME: "models/gemini-2.0-flash-exp",
